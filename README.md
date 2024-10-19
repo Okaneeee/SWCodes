1309e6cc2b609efa671e7d9cc5427b468f4a14eb
Anatole <anatole.giniaux@sfr.fr>
2024-10-19 23:23:46 +0200

    feat: unregisterid command

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ SW Codes is a discord bot to facilitate the use of codes on the mobile game Summ
 - [x] Add a command to show the list of all commands and descriptions (/help)
 - [x] Add a command to use a code on an account (/usecode 『hiveid』 『code』)[^5]
 - [ ] Add a command to register an ID to the list of IDs (/registerid 『hiveid』)[^1] [^5]
-- [ ] Add a command to unregister an ID from the list of IDs (/unregisterid 『hiveid』)[^5]
+- [x] Add a command to unregister an ID from the list of IDs (/unregisterid 『hiveid』)[^5]
 - [ ] Add a command to use a code on all registered IDs (/trigger 『code』)[^2]
 
 ### Features
-- [x] A JSON file to store the list of registered IDs (a db is overkill)[^3] [^4]
+- [x] A JSON file to store the list of registered IDs (a real db is overkill)[^3] [^4]
 
 ### Extra
 - [x] Change `token` to a `.env` file with token & ownerid
---
+++ src/commands/UnregisterID.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from utils.dbManager import removeID
+
+class UnregisterID(commands.Cog):
+    def __init__(self, bot : commands.Bot) -> None:
+        self.bot = bot
+        
+    @commands.slash_command(
+        name="unregisterid",
+        description="Unregister a Hive ID on the bot's database",
+        options=[
+            discord.Option(
+                type=discord.SlashCommandOptionType.string,
+                name="hiveid",
+                description="The Hive ID of the account",
+                required=True
+            )
+        ],
+        guild_ids=[1266543874562588797]
+    )
+    async def unregisterID(self, ctx, hiveid : str):
+        text: str = ""
+
+        resp: int = removeID(hiveid)
+
+        if resp == 200:
+            text = f"ID `{hiveid}` successfully removed"
+        elif resp == 404:
+            text = f"ID `{hiveid}` not found"
+        elif resp == 500:
+            text = "No registered IDs"
+        else:
+            text = "Unknown error, contact the developer"    
+
+        await ctx.respond(text, ephemeral=True)
+
+def setup(bot):
+    bot.add_cog(UnregisterID(bot))
